e85ad48eb6360a7991dd975e769b5809b77c0f0e
lcc <694625452@qq.com>
2024-05-15 17:09:25 +0800

    实时推理

--- GPT_SoVITS/generate_object.py
+++ GPT_SoVITS/generate_object.py
@@ -54,10 +54,7 @@ cnhubert_base_path = os.environ.get(
 bert_path = os.environ.get(
     "bert_path", "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
 )
-infer_ttswebui = os.environ.get("infer_ttswebui", 9872)
-infer_ttswebui = int(infer_ttswebui)
-is_share = os.environ.get("is_share", "False")
-is_share = eval(is_share)
+
 if "_CUDA_VISIBLE_DEVICES" in os.environ:
     os.environ["CUDA_VISIBLE_DEVICES"] = os.environ["_CUDA_VISIBLE_DEVICES"]
 
@@ -155,10 +152,6 @@ class BaseTorchModel:
         else:
             self.model_awake = True
         log.info(f'{self.name}模型已唤醒')
-    
-class BaseTransformerModel(BaseTorchModel):
-    def __init__(self,model_path,device):
-        super().__init__(model_path,device)
         
 class BertModel(BaseTorchModel):
     def __init__(self,model_path,device):
@@ -428,7 +421,7 @@ class Generator():
             self.final_dtype = torch.float32        
         
         
-    def get_tts_wav(self, ref_wav_path, prompt_text, prompt_language, text, text_language, how_to_cut=i18n("不切"), top_k=20, top_p=0.6, temperature=0.6, ref_free = False):
+    def get_tts_wav(self, ref_wav_path, prompt_text, prompt_language, text, text_language, how_to_cut=i18n("不切"), top_k=20, top_p=0.6, temperature=0.6, ref_free = False, stream=False):
         if prompt_text is None or len(prompt_text) == 0:
             ref_free = True
         t0 = ttime()
@@ -596,14 +589,17 @@ class Generator():
             audio_opt.append(audio)
             audio_opt.append(zero_wav)
             t4 = ttime()
+            if stream:
+                yield (np.concatenate([audio, zero_wav], 0) * 32768).astype(np.int16).tobytes()
         del prompt_semantic
         torch.cuda.empty_cache()
         self.gpt_model.sleep_model()
         self.sovits_model.sleep_model()
         print("%.3f\t%.3f\t%.3f\t%.3f" % (t1 - t0, t2 - t1, t3 - t2, t4 - t3))
-        yield self.sovits_model.hps.data.sampling_rate, (np.concatenate(audio_opt, 0) * 32768).astype(
-            np.int16
-        )
+        if not stream:
+            yield self.sovits_model.hps.data.sampling_rate, (np.concatenate(audio_opt, 0) * 32768).astype(
+                np.int16
+            )
 
     def split(self,todo_text):
         todo_text = todo_text.replace("……", "。").replace("——", "，")
@@ -719,113 +715,3 @@ gpt_model = GptModel(gpt_path,device)
 
 generator = Generator(sovits_model,gpt_model,bert_model,ssl_model)
 
-pretrained_sovits_name = "GPT_SoVITS/pretrained_models/s2G488k.pth"
-pretrained_gpt_name = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
-SoVITS_weight_root = "SoVITS_weights"
-GPT_weight_root = "GPT_weights"
-os.makedirs(SoVITS_weight_root, exist_ok=True)
-os.makedirs(GPT_weight_root, exist_ok=True)
-
-
-def get_weights_names():
-    SoVITS_names = [pretrained_sovits_name]
-    for name in os.listdir(SoVITS_weight_root):
-        if name.endswith(".pth"): SoVITS_names.append("%s/%s" % (SoVITS_weight_root, name))
-    GPT_names = [pretrained_gpt_name]
-    for name in os.listdir(GPT_weight_root):
-        if name.endswith(".ckpt"): GPT_names.append("%s/%s" % (GPT_weight_root, name))
-    return SoVITS_names, GPT_names
-
-def change_sovits_weights(*args, **kwargs):
-    pass
-
-def change_gpt_weights(*args, **kwargs):
-    pass
-
-def custom_sort_key(s):
-    # 使用正则表达式提取字符串中的数字部分和非数字部分
-    parts = re.split('(\d+)', s)
-    # 将数字部分转换为整数，非数字部分保持不变
-    parts = [int(part) if part.isdigit() else part for part in parts]
-    return parts
-
-def change_choices():
-    SoVITS_names, GPT_names = get_weights_names()
-    return {"choices": sorted(SoVITS_names, key=custom_sort_key), "__type__": "update"}, {"choices": sorted(GPT_names, key=custom_sort_key), "__type__": "update"}
-
-
-
-SoVITS_names, GPT_names = get_weights_names()
-
-with gr.Blocks(title="GPT-SoVITS WebUI") as app:
-    gr.Markdown(
-        value=i18n("本软件以MIT协议开源, 作者不对软件具备任何控制力, 使用软件者、传播软件导出的声音者自负全责. <br>如不认可该条款, 则不能使用或引用软件包内任何代码和文件. 详见根目录<b>LICENSE</b>.")
-    )
-    with gr.Group():
-        gr.Markdown(value=i18n("模型切换"))
-        with gr.Row():
-            GPT_dropdown = gr.Dropdown(label=i18n("GPT模型列表"), choices=sorted(GPT_names, key=custom_sort_key), value=gpt_path, interactive=True)
-            SoVITS_dropdown = gr.Dropdown(label=i18n("SoVITS模型列表"), choices=sorted(SoVITS_names, key=custom_sort_key), value=sovits_path, interactive=True)
-            refresh_button = gr.Button(i18n("刷新模型路径"), variant="primary")
-            refresh_button.click(fn=change_choices, inputs=[], outputs=[SoVITS_dropdown, GPT_dropdown])
-            SoVITS_dropdown.change(change_sovits_weights, [SoVITS_dropdown], [])
-            GPT_dropdown.change(change_gpt_weights, [GPT_dropdown], [])
-        gr.Markdown(value=i18n("*请上传并填写参考信息"))
-        with gr.Row():
-            inp_ref = gr.Audio(label=i18n("请上传3~10秒内参考音频，超过会报错！"), type="filepath")
-            with gr.Column():
-                ref_text_free = gr.Checkbox(label=i18n("开启无参考文本模式。不填参考文本亦相当于开启。"), value=False, interactive=True, show_label=True)
-                gr.Markdown(i18n("使用无参考文本模式时建议使用微调的GPT，听不清参考音频说的啥(不晓得写啥)可以开，开启后无视填写的参考文本。"))
-                prompt_text = gr.Textbox(label=i18n("参考音频的文本"), value="")
-            prompt_language = gr.Dropdown(
-                label=i18n("参考音频的语种"), choices=[i18n("中文"), i18n("英文"), i18n("日文"), i18n("中英混合"), i18n("日英混合"), i18n("多语种混合")], value=i18n("中文")
-            )
-        gr.Markdown(value=i18n("*请填写需要合成的目标文本和语种模式"))
-        with gr.Row():
-            text = gr.Textbox(label=i18n("需要合成的文本"), value="")
-            text_language = gr.Dropdown(
-                label=i18n("需要合成的语种"), choices=[i18n("中文"), i18n("英文"), i18n("日文"), i18n("中英混合"), i18n("日英混合"), i18n("多语种混合")], value=i18n("中文")
-            )
-            how_to_cut = gr.Radio(
-                label=i18n("怎么切"),
-                choices=[i18n("不切"), i18n("凑四句一切"), i18n("凑50字一切"), i18n("按中文句号。切"), i18n("按英文句号.切"), i18n("按标点符号切"), ],
-                value=i18n("凑四句一切"),
-                interactive=True,
-            )
-            with gr.Row():
-                gr.Markdown(value=i18n("gpt采样参数(无参考文本时不要太低)："))
-                top_k = gr.Slider(minimum=1,maximum=100,step=1,label=i18n("top_k"),value=5,interactive=True)
-                top_p = gr.Slider(minimum=0,maximum=1,step=0.05,label=i18n("top_p"),value=1,interactive=True)
-                temperature = gr.Slider(minimum=0,maximum=1,step=0.05,label=i18n("temperature"),value=1,interactive=True)
-            inference_button = gr.Button(i18n("合成语音"), variant="primary")
-            output = gr.Audio(label=i18n("输出的语音"))
-
-        inference_button.click(
-            generator.get_tts_wav,
-            [inp_ref, prompt_text, prompt_language, text, text_language, how_to_cut, top_k, top_p, temperature, ref_text_free],
-            [output],
-        )
-
-        gr.Markdown(value=i18n("文本切分工具。太长的文本合成出来效果不一定好，所以太长建议先切。合成会根据文本的换行分开合成再拼起来。"))
-        with gr.Row():
-            text_inp = gr.Textbox(label=i18n("需要合成的切分前文本"), value="")
-            button1 = gr.Button(i18n("凑四句一切"), variant="primary")
-            button2 = gr.Button(i18n("凑50字一切"), variant="primary")
-            button3 = gr.Button(i18n("按中文句号。切"), variant="primary")
-            button4 = gr.Button(i18n("按英文句号.切"), variant="primary")
-            button5 = gr.Button(i18n("按标点符号切"), variant="primary")
-            text_opt = gr.Textbox(label=i18n("切分后文本"), value="")
-            button1.click(generator.cut1, [text_inp], [text_opt])
-            button2.click(generator.cut2, [text_inp], [text_opt])
-            button3.click(generator.cut3, [text_inp], [text_opt])
-            button4.click(generator.cut4, [text_inp], [text_opt])
-            button5.click(generator.cut5, [text_inp], [text_opt])
-        gr.Markdown(value=i18n("后续将支持转音素、手工修改音素、语音合成分步执行。"))
-
-app.queue(concurrency_count=511, max_size=1022).launch(
-    server_name="0.0.0.0",
-    inbrowser=True,
-    share=is_share,
-    server_port=infer_ttswebui,
-    quiet=True,
-)
---
+++ new_api.py
@@ -0,0 +1,122 @@
+import json
+import io
+import wave
+from fastapi import FastAPI,Body
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+import uvicorn
+import time
+import torch
+from GPT_SoVITS.generate_object import BertModel,CnhubertModel,SovitsModel,GptModel,Generator,cnhubert_base_path,bert_path,device,i18n
+
+app = FastAPI()
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+bert_device = device
+ssl_device = device
+sovits_device = device
+gpt_device = device    
+
+bert_model = BertModel(bert_path,bert_device)
+
+ssl_model = CnhubertModel(cnhubert_base_path,ssl_device)
+
+generator = None
+
+class SpeakWordRequest(BaseModel):
+    voice_id: str
+    word: str
+    language: str
+    byte_stream: bool = True
+    wave_head: bool = True
+    how_to_cut: str = '不切'
+    top_k: int = 5
+    top_p: float = 1
+    temperature: float = 1
+
+
+def wave_header_chunk(frame_input=b"", channels=1, sample_width=2, sample_rate=32000):
+    wav_buf = io.BytesIO()
+    with wave.open(wav_buf, "wb") as vfout:
+        vfout.setnchannels(channels)
+        vfout.setsampwidth(sample_width)
+        vfout.setframerate(sample_rate)
+        vfout.writeframes(frame_input)
+    wav_buf.seek(0)
+    return wav_buf.read()
+
+async def generate_tts_flow(generator,word,language,top_k,top_p,temperature,wave_head):
+    if generator.last_gen != word:
+        generator.last_gen = word
+        chunks = generator.get_tts_wav(ref_wav_path=generator.prompt_info['ref_wav_path'],
+                            prompt_text=generator.prompt_info['prompt_text'],
+                            prompt_language=generator.prompt_info['prompt_language'],
+                            text=word,
+                            text_language=language,
+                            how_to_cut=i18n('不切'),
+                            top_k=top_k,
+                            top_p=top_p,
+                            temperature=temperature,
+                            ref_free=False,
+                            stream=True)
+        if wave_head:
+            yield wave_header_chunk()
+            print("Head 发送")
+        for chunk in chunks:
+            yield chunk
+    yield b''
+
+@app.post('/tts_flow')
+def tts_flow(input_params:SpeakWordRequest = Body(...)):
+    global generator
+    if generator == None:
+        generator = init_generator(input_params.voice_id)
+    if generator.voice_id != input_params.voice_id:
+        # 更换声音
+        del generator
+        torch.cuda.empty_cache()
+        generator = init_generator(input_params.voice_id)
+    return StreamingResponse(generate_tts_flow(generator))
+
+
+@app.get('/tts_flow_test')
+async def tts_flow_test(text:str):
+    voice_id = '276'
+    global generator
+    if generator == None:
+        generator = init_generator(voice_id)
+    if generator.voice_id != voice_id:
+        # 更换声音
+        del generator
+        torch.cuda.empty_cache()
+        generator = init_generator(voice_id)
+    return StreamingResponse(generate_tts_flow(generator,text,i18n("中英混合"),5,1,1,True), media_type="audio/x-wav")
+
+
+def get_model_info(voice_id):
+    
+    sovits_path = '/root/GPT-SoVITS-Next/SoVITS_weights/276.pth'
+    gpt_path = '/root/GPT-SoVITS-Next/GPT_weights/276.ckpt'
+    prompt_info = {'ref_wav_path':'/root/GPT-SoVITS-Next/notype1 (1).wav',
+                   'prompt_text':'请帮我看一眼这封邮件，请给我来一份招牌菜。',
+                   'prompt_language':i18n("中英混合"),
+                   }
+    return sovits_path,gpt_path,prompt_info
+
+def init_generator(voice_id):
+    sovits_path,gpt_path,prompt_info = get_model_info(voice_id)
+    sovits_model = SovitsModel(sovits_path,device)
+    gpt_model = GptModel(gpt_path,device)
+    generator = Generator(sovits_model,gpt_model,bert_model,ssl_model)
+    generator.voice_id = voice_id
+    generator.prompt_info = prompt_info
+    generator.last_gen = ''
+    del sovits_model,gpt_model
+    torch.cuda.empty_cache()
+    return generator
+
+if __name__ == '__main__':
+    uvicorn.run(app='new_api:app', reload=True, host='0.0.0.0', port=8000)
